29b3cc3dab7c987bdd631825bf7bcd876d4d082f
VAngelina <angelina.vaa@gmail.com>
2019-12-09 10:55:19 +0400

    Реализация Лассо регрессии

--- regression.py
+++ regression.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression, Ridge
+from sklearn.linear_model import Ridge, Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-
 
 # Чтение файла с данными
 dataset = pd.read_csv('ENB2012_data.csv', ';', decimal=',')
@@ -21,31 +20,35 @@ dataset = dataset.drop(['X1', 'X4'], axis=1)
 # Зададим данные для обучения и предсказания
 target_data = dataset['Y1']
 train_data = dataset.drop(['Y1', 'Y2'], axis=1)
-
-logistic_model = LogisticRegression()
-ridge_model = Ridge(alpha=1.0)
 X_train, X_test, Y_train, Y_test = train_test_split(
     train_data, target_data, test_size=0.4)
 
-# Обучаем модели
-logistic_model.fit(X_train, Y_train.astype(int))
+# Создадим модели
+lasso_model = Lasso(alpha=0.01)
+ridge_model = Ridge(alpha=0.01)
+lasso_model.fit(X_train, Y_train)
 ridge_model.fit(X_train, Y_train)
+# Посчитаем коэффициенты уравнения полученной модели регрессии
+print(lasso_model.coef_)
+print(ridge_model.coef_)
 
 # Предсказываем значения
-logistic_predict = logistic_model.predict(X_test)
+lasso_predict = lasso_model.predict(X_test)
 ridge_predict = ridge_model.predict(X_test)
+print(lasso_predict)
+print(ridge_predict)
 
 # Вычисляем коэффициент детерминации
-r2_logistic = r2_score(Y_test, logistic_predict)
+r2_lasso = r2_score(Y_test, lasso_predict)
 r2_ridge = r2_score(Y_test, ridge_predict)
-print(r2_logistic)
+print(r2_lasso)
 print(r2_ridge)
 
 # Строим графики спрогнозированных значений
 fig, axes = plt.subplots(2, 1)
 axes[0].set_title('Гребневая регрессия')
-axes[1].set_title('Логистическая регрессия')
-predicts = (logistic_predict, ridge_predict)
+axes[1].set_title('Лассо регрессия')
+predicts = (lasso_predict, ridge_predict)
 for i, predict in enumerate(predicts):
     for x in X_test:
         axes[i].scatter(X_test[x].values, predict, s=5, label=x)
